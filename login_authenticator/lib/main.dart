f280bf9662aff971f6d4c4a86cd14ef414396dcb
BrBoomr <jafet.reyes01@utrgv.edu>
2019-02-27 10:10:36 -0600

    replaced login_authenticator

--- login_authenticator/lib/main.dart
+++ login_authenticator/lib/main.dart
@@ -1,26 +1,74 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart';
-import 'home_page.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final routes = <String, WidgetBuilder>{
-    LoginPage.tag: (context) => LoginPage(),
-    HomePage.tag: (context) => HomePage(),
-  };
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kodeversitas',
       debugShowCheckedModeBanner: false,
+      title: 'Login Authenticator',
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-        fontFamily: 'Nunito',
+        brightness: Brightness.dark,
+        accentColor: Colors.red,
       ),
-      home: LoginPage(),
-      routes: routes,
+      home: LoginPage(title: 'Login Page'),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  String url = "sleepy-stream-87265.herokuapp.com";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+            ),
+            TextField(
+              controller: usernameController,
+              decoration: InputDecoration(
+                  hintText: "Username",
+                  contentPadding: const EdgeInsets.all(20)),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                  hintText: "Password",
+                  contentPadding: const EdgeInsets.all(20)),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+            ),
+            FloatingActionButton.extended(
+              onPressed: () {},
+              label: Text("Submit"),
+              icon: Icon(Icons.forward),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
